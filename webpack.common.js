a0415a33ca3043848cd4e47efd649472d5588000
Matt Severyn <mseveryn@gmail.com>
2020-09-12 23:28:44 -0700

    Webpack is the devil

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendor',
-          chunks: 'all',
+          // chunks: 'all',
         },
       },
     },
